a6b5469dfbb0942f790c5ed367d2b5c77eb32efb
Evgenij00 <49844107+Evgenij00@users.noreply.github.com>
2021-02-18 13:54:03 +0300

    connect mongoDb

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
 
 async function start() {
     try {
-        await mongoose.connect('', {
+        await mongoose.connect('mongodb+srv://evgeny:ZAQ!2wsx@cluster0.cyfck.mongodb.net/todos', {
             useNewUrlParser: true,
-            useFindAndModify: false,
+            // useFindAndModify: false,
+            useUnifiedTopology: true,
         })
         app.listen(PORT, () => {
             console.log('Server has been started...');
